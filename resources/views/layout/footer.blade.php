f6e6ca7b7e67544abbc96ce55b15edef312188c9
dev <badjilaminefbkk@gmail.com>
2019-11-15 17:48:31 +0000

    premiercommit

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,29 @@
+
+      <footer class="footer">
+        <div class="container-fluid">
+          <ul class="nav">
+            <li class="nav-item">
+              <a href="javascript:void(0)" class="nav-link">
+                A PROPOS
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="javascript:void(0)" class="nav-link">
+                SUIVI-INSERTION
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="javascript:void(0)" class="nav-link">
+                
+              </a>
+            </li>
+          </ul>
+          <div class="copyright">
+            ©
+            <script>
+              document.write(new Date().getFullYear())
+            </script> ONFP <i class=""></i> by
+            <a href="javascript:void(0)" target="_blank">www.onfp.sn</a>Tous droits réservés.
+          </div>
+        </div>
+      </footer>
